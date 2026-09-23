a57326e6354b745cbfe796ba3817760662236fcc
Watcharin Sirigate <watcharin.s@freshket.co>
2026-09-23 08:16:12 +0000

    init

---
+++ models/marts/monthly_sales.sql
@@ -0,0 +1,10 @@
+SELECT
+   DATE_TRUNC('month', order_date) AS order_month,
+   market_segment,
+   SUM(gross_item_sales_amount) AS total_revenue
+FROM
+   {{ ref('fct_orders') }}
+GROUP BY
+   1, 2
+ORDER BY
+   1, 2
